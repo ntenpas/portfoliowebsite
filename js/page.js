1c5cfc698cd3e4c4c33f217a5561a7be8ad8e04d
Nolan Tenpas <ntenpasdeveloper@gmail.com>
2015-06-06 18:56:22 -0500

    Rotation change adjusts menu.

--- js/page.js
+++ js/page.js
@@ -1,5 +1,8 @@
 var hamburger = document.getElementById('hamburger');
 var nav = document.getElementById('nav');
+var blogTab = document.getElementById('blogTab');
+var resumeTab = document.getElementById('resumeTab');
+var aboutTab = document.getElementById('aboutTab');
 var html = document.getElementById('html');
 var baseFontSize = window.getComputedStyle(html, null)['font-size'];
 var remSize = parseFloat(baseFontSize);
@@ -11,10 +14,37 @@ var checkSmallScreen = function() {
 	}
 }
 var checkLargeScreen = function() {
+	if (window.innerWidth > window.innerHeight) {
+		nav.style.visibility = 'visible';
+		hamburger.style.display = 'none';
+	}
 	if (nav.clientHeight == Math.round(4*remSize)) {
 		nav.style.visibility = 'visible';
 		hamburger.style.display = 'none';
 	}
+}
+var getContent = function(event, pageName) {
+	event.preventDefault();
+
+	// get content of blog page, and display it
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			var newContent = xhr.responseXML.getElementById('content');
+			var page = document.getElementById('page');
+
+			// remove old content
+			while (page.firstChild) {
+				page.removeChild(page.firstChild);
+			}
+
+			// append the new content
+			page.appendChild(newContent);
+		}
+	}
+	xhr.open('GET', '../' + pageName + '.html');
+	xhr.responseType = 'document';
+	xhr.send();
 }
 
 // if the screen is loaded small, nav below banner
@@ -22,9 +52,16 @@ checkSmallScreen();
 
 // account for window resizing
 window.addEventListener('resize', function(event) {
-	checkSmallScreen();
 	checkLargeScreen();
+	checkSmallScreen();
 });
+
+// account for device rotations
+//window.screen.addEventListener('orientationchange', function(event) {
+	//alert('change!');
+	//checkSmallScreen();
+	//checkLargeScreen();
+//});
 
 // on hamburger menu click, display nav
 hamburger.addEventListener('click', function(event) {
@@ -38,4 +75,15 @@ hamburger.addEventListener('click', function(event) {
 	else if (nav.style.visibility == 'visible') {
 		nav.style.visibility = 'hidden';
 	}
+});
+
+// asynchronous page loads
+blogTab.addEventListener('click', function(event) {
+	getContent(event, 'index');
+});
+resumeTab.addEventListener('click', function(event) {
+	getContent(event, 'resume');
+});
+aboutTab.addEventListener('click', function(event) {
+	getContent(event, 'about');
 });
